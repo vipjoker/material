5c71f4f00c66f2b18ca97a77a26ca92d390d063d
oleh <tajcig@ya.ru>
2016-08-28 02:49:29 +0300

    added pictures

--- js/init.js
+++ js/init.js
@@ -3,6 +3,18 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    
+    
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function openDialog(event) {
+  $('#modal1').openModal();
+  // $('#modal1').closeModal();
+}
+
+function openBottomSheet(event) {
+  $('#modal2').openModal();
+}
